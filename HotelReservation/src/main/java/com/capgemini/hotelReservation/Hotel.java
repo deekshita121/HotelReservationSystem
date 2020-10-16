6ee2098cbc6c0a0467582479f82a4f573adba0e2
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-16 20:14:08 +0530

    [deekshita] Add : Ability to add hotel details

--- HotelReservation/src/main/java/com/capgemini/hotelReservation/Hotel.java
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/Hotel.java
@@ -2,4 +2,33 @@ package com.capgemini.hotelReservation;
 
 public class Hotel {
 
+	private String name;
+	private int rate;
+
+	public Hotel(String name, int rate) {
+		super();
+		this.name = name;
+		this.rate = rate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel : \nName = " + name + "\nRate = $" + rate + "\n";
+	}
 }
--- HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
@@ -1,14 +1,25 @@
 package com.capgemini.hotelReservation;
 
-/**
- * Hello world!
- *
- */
-public class HotelReservation 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-    }
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HotelReservation {
+	private static final Logger log = LogManager.getLogger(HotelReservation.class);
+	static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+
+	public static void main(String[] args) {
+		HotelReservation hotelReservation = new HotelReservation();
+
+		Hotel h1 = new Hotel("Lakewood", 110);
+		Hotel h2 = new Hotel("Brigewood", 160);
+		Hotel h3 = new Hotel("Ridgewood", 220);
+
+		hotelList.add(h1);
+		hotelList.add(h2);
+		hotelList.add(h3);
+		hotelReservation.hotelList.forEach(n -> log.info(n));
+
+	}
 }
