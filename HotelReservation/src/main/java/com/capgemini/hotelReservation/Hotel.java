e6604f4561247358a902b6b404049494e2968b6e
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-16 19:36:40 +0530

    [deekshita] Add : Welcome

---
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/Hotel.java
@@ -0,0 +1,5 @@
+package com.capgemini.hotelReservation;
+
+public class Hotel {
+
+}
---
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
@@ -0,0 +1,14 @@
+package com.capgemini.hotelReservation;
+
+/**
+ * Hello world!
+ *
+ */
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
