fb2da3a8a6130f053438fc31824c5f6f0a9fca32
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-16 21:57:12 +0530

    [deekshita] Add : Ability to allot user a cheap hotel

--- HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
@@ -1,16 +1,20 @@
 package com.capgemini.hotelReservation;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class HotelReservation {
 	private static final Logger log = LogManager.getLogger(HotelReservation.class);
+	private static Scanner sc = new Scanner(System.in);
 	static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
 
-	public static void main(String[] args) {
-		HotelReservation hotelReservation = new HotelReservation();
+	/**
+	 * 
+	 */
+	public void addHotels() {
 
 		Hotel h1 = new Hotel("Lakewood", 110);
 		Hotel h2 = new Hotel("Brigewood", 160);
@@ -19,7 +23,32 @@ public class HotelReservation {
 		hotelList.add(h1);
 		hotelList.add(h2);
 		hotelList.add(h3);
+	}
+
+	/**
+	 * 
+	 */
+	public void cheapHotel() {
+		log.info("Enter dates in the format with a comma (dd mmm yyy) ");
+		String date = sc.nextLine();
+		String days[] = date.split(",");
+		int minCost = hotelList.get(0).getRate();
+		String cheapHotelName = hotelList.get(0).getName();
+		for (int i = 1; i < hotelList.size(); i++)
+			if (hotelList.get(i).getRate() < minCost) {
+				minCost = hotelList.get(i).getRate();
+				cheapHotelName = hotelList.get(i).getName();
+			}
+		log.info(cheapHotelName + ", Total Rates: $" + minCost);
+
+	}
+
+	public static void main(String[] args) {
+		HotelReservation hotelReservation = new HotelReservation();
+
+		hotelReservation.addHotels();
 		hotelReservation.hotelList.forEach(n -> log.info(n));
+		hotelReservation.cheapHotel();
 
 	}
 }
