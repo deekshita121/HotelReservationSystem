4bd198211369deb65e1db66b0746ac25603283c6
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-16 23:39:42 +0530

    [deekshita] Add : Ability to allot cheap hotel

--- HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/HotelReservation.java
@@ -1,7 +1,15 @@
 package com.capgemini.hotelReservation;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,17 +37,35 @@ public class HotelReservation {
 	 * 
 	 */
 	public void cheapHotel() {
-		log.info("Enter dates in the format with a comma (dd mmm yyy) ");
-		String date = sc.nextLine();
-		String days[] = date.split(",");
-		int minCost = hotelList.get(0).getWeekdayRate();
-		String cheapHotelName = hotelList.get(0).getName();
-		for (int i = 1; i < hotelList.size(); i++)
-			if (hotelList.get(i).getWeekdayRate() < minCost) {
-				minCost = hotelList.get(i).getWeekdayRate();
-				cheapHotelName = hotelList.get(i).getName();
+
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MM yyyy");
+		log.info("Enter start date (dd MM yyyy): ");
+		String start = sc.nextLine();
+		log.info("Enter end date (dd MM yyyy): ");
+		String end = sc.nextLine();
+		LocalDate startDate = LocalDate.parse(start, dateFormat);
+		LocalDate endDate = LocalDate.parse(end, dateFormat);
+		int noOfDays = (int) ChronoUnit.DAYS.between(startDate, endDate);
+		DayOfWeek startDay = startDate.getDayOfWeek();
+		DayOfWeek endDay = endDate.getDayOfWeek();
+		long daysWithoutWeekends = noOfDays - 2 * ((noOfDays + startDay.getValue()) / 7);
+		long noOfWeekdays = daysWithoutWeekends + (startDay.getValue() == 1 ? 1 : 0) + (endDay.getValue() == 1 ? 1 : 0);
+		long noOfWeekends = noOfDays - (noOfWeekdays);
+		
+		long minCost = hotelList.get(0).getWeekdayRate() * noOfWeekdays
+				+ hotelList.get(0).getWeekendRate() * noOfWeekends;
+		String cheapHotel = hotelList.get(0).getName();
+
+		for (int i = 1; i < hotelList.size(); i++) {
+			if (hotelList.get(i).getWeekdayRate() * noOfWeekdays
+					+ hotelList.get(i).getWeekendRate() * noOfWeekends < minCost) {
+				minCost = hotelList.get(i).getWeekdayRate() * noOfWeekdays
+						+ hotelList.get(i).getWeekendRate() * noOfWeekends;
+				cheapHotel = hotelList.get(i).getName();
 			}
-		log.info(cheapHotelName + ", Total Rates: $" + minCost*days.length);
+		}
+
+		log.info(cheapHotel + ", Total rate :$" + minCost);
 
 	}
 
