74c541a2adc4689d90a6bbc8294f66fc2ffc7659
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-17 18:08:49 +0530

    [deekshita] Add : Add regular expressions and throw invalid exception

--- HotelReservation/src/main/java/com/capgemini/hotelReservation/InvalidInputException.java
+++ HotelReservation/src/main/java/com/capgemini/hotelReservation/InvalidInputException.java
@@ -1,6 +1,6 @@
 package com.capgemini.hotelReservation;
 
-public class InvalidInputException extends Exception{
+public class InvalidInputException extends Exception {
 
 	public InvalidInputException(String message) {
 		super(message);
